57db59841c8de37d9a5f0a917975c200bd81f090
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-11-18 21:21:34 -0300

    1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 let data = new Date()
 let hoje = data.toLocaleDateString()
 let hora = data.toLocaleTimeString()
+let ano = data.getFullYear()
 
 let dataDocumento = document.querySelector("#data");
 dataDocumento.innerHTML = `Você acessou este site no dia ${hoje}, às ${hora}.`
+
+let nascimento = 1985
+let atual = ano.valueOf()
+let idade =  atual - nascimento
+
+let sobreMim = document.querySelector("#sobreMim");
+sobreMim.innerHTML = `Olá, eu me chamo Lauro e tenho ${idade} anos de idade. Minha idade que aparece aqui é a real, pois foi adicionada dinamicamente através do javascript.`
